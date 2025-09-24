237e483d07e164801aa0be171fdd248b92db6be4
lubbino <mr.lubbin@gmail.com>
2025-09-24 09:41:04 +0200

    Implement personal information display in Program.cs

--- exercises/03-variables/01-personal-info/Program.cs
+++ exercises/03-variables/01-personal-info/Program.cs
@@ -9,8 +9,29 @@ using System;
 // Example:
 // string firstName = "Your Name";
 
+//Basic information about human writing this program.
+string firstName = "Ludvig";
+string lastName = "Lifbom";
+int age = 18;
+int favoriteNumber = 30;
+bool isStudent = true;
+double height = 181.1;
+
+//Current year variable.
+int currentYear = 2025;
+
+//Birth year
+int birthYear = currentYear - age;
+
+
 Console.WriteLine("Personal Information");
 Console.WriteLine("===================");
+Console.WriteLine("Name: " + firstName + " " + lastName);
+Console.WriteLine("Age: " + age + " years old.");
+Console.WriteLine("Birth year: " + birthYear);
+Console.WriteLine("Height: " + height + " cm.");
+Console.WriteLine("Student: " + isStudent);
+Console.WriteLine("Favorite Number: " + favoriteNumber);
 
 // TODO: Display the information using the variables
 // Format: "Name: " + firstName + " " + lastName
